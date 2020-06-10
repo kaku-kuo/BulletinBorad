c3c02faca8aefef2f6d0500ec2e80c1dcbf2b9c2
kaku-kuo <z22857744@gmail.com>
2020-06-10 17:18:44 +0800

    ready for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path');
 
 const app = express();
 
@@ -9,7 +10,7 @@ connectDB();
 // Init Middleware
 app.use(express.json({extended:false}));
 
-app.get('/', (req,res) => res.json({msg:'welcome'}));
+
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
@@ -17,6 +18,10 @@ app.use('/api/posts', require('./routes/posts'));
 app.use('/api/comments', require('./routes/comments'));
 app.use('/api/auth', require('./routes/auth'));
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+    app.get('*',(req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => console.log(`Server started on port ${PORT}`)); 
